64128d055fad844df695b2b37340a5fcb1dcd7fc
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-07-13 00:23:17 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
     <Provider store={store} >
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/GooseTrack-Frontend">
+        <BrowserRouter basename="/react_node_fs65_Larik7">
           <TourProvider steps={steps} startAt={0}>
             <App />
           </TourProvider>
